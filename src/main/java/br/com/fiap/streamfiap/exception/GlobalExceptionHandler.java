22321a6f460e14111b3e2f51789f94a367992959
RAFAEL LOUZÃ LOPES <rm564963@fiap.com.br>
2026-09-08 15:49:40 -0300

    fix: bug07 - preserva mensagem de duracao invalida

--- src/main/java/br/com/fiap/streamfiap/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/fiap/streamfiap/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package br.com.fiap.streamfiap.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -34,4 +35,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, String>> handleArgumentoInvalido(IllegalArgumentException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("erro", e.getMessage()));
     }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleMensagemInvalida(
+            HttpMessageNotReadableException e) {
+
+        Throwable causa = e.getMostSpecificCause();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("erro", causa.getMessage()));
+    }
+
 }
